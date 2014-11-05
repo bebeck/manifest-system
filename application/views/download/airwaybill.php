ccc8114d0b823a61f3a8b093a5469699ff812bda
bebeck <bebeckproject@gmail.com>
2014-11-05 23:40:22 +0700

    invoice

--- application/views/download/airwaybill.php
+++ application/views/download/airwaybill.php
@@ -2,83 +2,118 @@
 <head>
 <title>Airway Bill</title>
 <link rel="stylesheet" href="<?=base_url() ?>style/css/airwaybill.css">
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 </head>
 
 <body>
 
-<div class="content">
-
-	<div class="header">
-		<div class="logo">
-			<img src="<?=base_url()?>asset/images/tata-logo.png">
-		</div>
-		<div class="description">
-			<strong>Shipment Airwaybill</strong><br/><br/><br/>
-			(Non negotiable)
-		</div>
-		<div class="qrcode">
-			<img src="<?=base_url()?>asset/images/qr-code.png">
-		</div>
-	</div>
-
-	<div class="body-content">
-		<div class="information-shipping">
-			<div class="shipping-details info-sender">
-				<div class="title">SHIPPER</div>
-				<div class="shipping-content">
-				<table border="0">
-					<tr>
-						<td width="70">Name</td>
-						<td>:</td>
-						<td>PT. Abc Indonesia Raya</td>
-					</tr>
-					<tr>
-						<td width="50">Address</td>
-						<td>:</td>
-						<td>Jl. Kebayoran Lama Rt.xx Rw.xx No.xx Kebayoran Lama Jakarta Selatan Indonesia</td>
-					</tr>
-					<tr>
-						<td width="50">Telp/Hp</td>
-						<td>:</td>
-						<td>0123-456-789</td>
-					</tr>
-					<tr>
-						<td width="50">Attn</td>
-						<td>:</td>
-						<td></td>
-					</tr>
-				</table>
-				</div>
-			</div>
-			<div class="shipping-details info-consignee">
-				<div class="title">CONSIGNEE</div>
-				<div class="shipping-content">
-				<table border="0">
-					<tr>
-						<td width="70">Name</td>
-						<td>:</td>
-						<td>PT. Abc Indonesia Raya</td>
-					</tr>
-					<tr>
-						<td width="50">Address</td>
-						<td>:</td>
-						<td>Jl. Kebayoran Lama Rt.xx Rw.xx No.xx Kebayoran Lama Jakarta Selatan Indonesia</td>
-					</tr>
-					<tr>
-						<td width="50">Telp/Hp</td>
-						<td>:</td>
-						<td>0123-456-789</td>
-					</tr>
-					<tr>
-						<td width="50">Attn</td>
-						<td>:</td>
-						<td></td>
-					</tr>
-				</table>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+    		<div class="invoice-title">
+    			<h2>Invoice</h2><h3 class="pull-right">Order # 12345</h3>
+    		</div>
+    		<hr>
+    		<div class="row">
+    			<div class="col-xs-6">
+    				<address>
+    				<strong>Billed To:</strong><br>
+    					John Smith<br>
+    					1234 Main<br>
+    					Apt. 4B<br>
+    					Springfield, ST 54321
+    				</address>
+    			</div>
+    			<div class="col-xs-6 text-right">
+    				<address>
+        			<strong>Shipped To:</strong><br>
+    					Jane Smith<br>
+    					1234 Main<br>
+    					Apt. 4B<br>
+    					Springfield, ST 54321
+    				</address>
+    			</div>
+    		</div>
+    		<div class="row">
+    			<div class="col-xs-6">
+    				<address>
+    					<strong>Payment Method:</strong><br>
+    					Visa ending **** 4242<br>
+    					jsmith@email.com
+    				</address>
+    			</div>
+    			<div class="col-xs-6 text-right">
+    				<address>
+    					<strong>Order Date:</strong><br>
+    					March 7, 2014<br><br>
+    				</address>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    
+    <div class="row">
+    	<div class="col-md-12">
+    		<div class="panel panel-default">
+    			<div class="panel-heading">
+    				<h3 class="panel-title"><strong>Order summary</strong></h3>
+    			</div>
+    			<div class="panel-body">
+    				<div class="table-responsive">
+    					<table class="table table-condensed">
+    						<thead>
+                                <tr>
+        							<td><strong>Item</strong></td>
+        							<td class="text-center"><strong>Price</strong></td>
+        							<td class="text-center"><strong>Quantity</strong></td>
+        							<td class="text-right"><strong>Totals</strong></td>
+                                </tr>
+    						</thead>
+    						<tbody>
+    							<!-- foreach ($order->lineItems as $line) or some such thing here -->
+    							<tr>
+    								<td>BS-200</td>
+    								<td class="text-center">$10.99</td>
+    								<td class="text-center">1</td>
+    								<td class="text-right">$10.99</td>
+    							</tr>
+                                <tr>
+        							<td>BS-400</td>
+    								<td class="text-center">$20.00</td>
+    								<td class="text-center">3</td>
+    								<td class="text-right">$60.00</td>
+    							</tr>
+                                <tr>
+            						<td>BS-1000</td>
+    								<td class="text-center">$600.00</td>
+    								<td class="text-center">1</td>
+    								<td class="text-right">$600.00</td>
+    							</tr>
+    							<tr>
+    								<td class="thick-line"></td>
+    								<td class="thick-line"></td>
+    								<td class="thick-line text-center"><strong>Subtotal</strong></td>
+    								<td class="thick-line text-right">$670.99</td>
+    							</tr>
+    							<tr>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								<td class="no-line text-center"><strong>Shipping</strong></td>
+    								<td class="no-line text-right">$15</td>
+    							</tr>
+    							<tr>
+    								<td class="no-line"></td>
+    								<td class="no-line"></td>
+    								<td class="no-line text-center"><strong>Total</strong></td>
+    								<td class="no-line text-right">$685.99</td>
+    							</tr>
+    						</tbody>
+    					</table>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 </div>
 
 </body>
